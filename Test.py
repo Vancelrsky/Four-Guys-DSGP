4a5f025e3c015f7f840b466a158ca9ecfab80cae
VanceeLi <vancelrsky@gmail.com>
2023-03-14 18:43:17 +0000

    applied pca to all training data

--- Test.py
+++ Test.py
@@ -1,5 +1,7 @@
-import MyNoteBook
+get_ipython().run_line_magic('run', 'MyNoteBook.py')
 import Valid_Datasets
+from sklearn.decomposition import PCA
+import pandas as pd
 instance = Valid_Datasets.get_df('00EABED2-271D-49D8-B599-1D4A09240601')
 
 test_example = instance
@@ -9,3 +11,13 @@ cleaned_data = Valid_Datasets.KNN_for_watch_data(cleaned_data,10)
 cleaned_data = Valid_Datasets.pca_to_data(cleaned_data,2)
 cleaned_data
 print(Valid_Datasets.get_cross_validation('test',0))
+import gzip
+import pandas as pd
+
+uuid = '00EABED2-271D-49D8-B599-1D4A09240601'
+user_data_file = 'Datasets/%s.features_labels.csv.gz' % uuid
+with gzip.open(user_data_file,'rb') as fid:
+    csv_df = pd.read_csv(fid,delimiter=',', index_col= 0)
+    csv_df = pd.DataFrame(csv_df,index=pd.MultiIndex.from_product([[uuid],csv_df.index]))
+    pass
+csv_df
--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,9 @@
 import os 
 os.system('Python MyNoteBook.py')
 import Valid_Datasets
+import pandas as pd
 
+""" 
 uuid_list = []
 f = open('UUID List.txt', 'r')
 for line in f.readlines():
@@ -17,4 +19,22 @@ for uuid in uuid_list:
         cleaned_data = Valid_Datasets.pca_to_data(cleaned_data,2)
         cleaned_data.to_csv('Cleaned_data/%s.csv'%uuid,mode = 'w')
     except:
-        print(uuid, 'failed to output')
+        print(uuid, 'failed to output') 
+"""
+
+uuid_list = Valid_Datasets.get_cross_validation('train',0)
+valid_data = pd.DataFrame()
+for uuid in uuid_list:
+    df = Valid_Datasets.get_df(uuid)
+    try:
+        cleaned_data = Valid_Datasets.non_watch_value_imputer(df)
+        cleaned_data = Valid_Datasets.KNN_for_watch_data(cleaned_data,10)
+        muti_index = pd.MultiIndex.from_product([[uuid], cleaned_data.index], names=['uuid','timestamps'])
+        cleaned_data = pd.DataFrame(cleaned_data.values, columns=cleaned_data.columns, index=muti_index)
+        valid_data = pd.concat([valid_data,cleaned_data],axis=0,ignore_index=False)
+    except:
+        print(uuid,'failed to export')
+
+
+valid = Valid_Datasets.pca_to_data(valid_data,2)
+valid.to_csv('train_data.csv',mode = 'w')
